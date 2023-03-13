b2790d7eead0fcdc255d3a9c636977125f422739
ademclk <ademonurcelik@icloud.com>
2023-03-14 01:09:19 +0300

    Add: Api paths

---
+++ Ceyehat/Utility/Api.swift
@@ -0,0 +1,14 @@
+//
+//  Api.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 13.03.2023.
+//
+
+import Foundation
+
+struct Api {
+    static let BASE_URL = "http://localhost:5228/api/"
+    static let GET_AIRPORTS = "Airport/search"
+    static let SEARCH_AIRPORTS = "Airport/search?searchTerm="
+}
