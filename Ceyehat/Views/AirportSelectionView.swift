95a33d3af3ec55af96cc07954c1199faf3837119
ademclk <ademonurcelik@icloud.com>
2023-03-14 02:10:25 +0300

    Improvement: Airport selection

--- Ceyehat/Views/AirportSelectionView.swift
+++ Ceyehat/Views/AirportSelectionView.swift
@@ -9,24 +9,36 @@ import SwiftUI
 
 struct AirportSelectionView: View {
     @StateObject var viewModel = AirportViewModel()
+    @Binding var selectedAirport: Airport?
+    
     var body: some View {
         NavigationView() {
             List {
                 ForEach(viewModel.airports, id: \.self) { airport in
-                    HStack {
-                        RoundedRectangle(cornerRadius: 8)
-                            .fill(Color.blue)
-                            .frame(width: 60, height: 60)
-                            .overlay(Text(airport.iataCode)
-                                .bold()
-                                .foregroundColor(.white))
-                        VStack(alignment: .leading) {
-                            Text("\(airport.cityName), \(airport.countryName)")
-                                .font(.headline)
-                            Text("\(airport.name)")
-                                .font(.subheadline)
+                    Button(action: {
+                        selectedAirport = airport
+                    }) {
+                        HStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(selectedAirport == airport ? Color.red : Color.blue)
+                                .frame(width: 60, height: 60)
+                                .overlay(Text(airport.iataCode)
+                                    .bold()
+                                    .foregroundColor(.white))
+                            VStack(alignment: .leading) {
+                                Text("\(airport.cityName), \(airport.countryName)")
+                                    .font(.headline)
+                                    .foregroundColor(selectedAirport == airport ? Color.red : Color.primary)
+                                Text("\(airport.name)")
+                                    .font(.subheadline)
+                                    .foregroundColor(selectedAirport == airport ? Color.red : Color.primary)
+                            }
+                            Spacer()
+                            if selectedAirport == airport {
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(selectedAirport == airport ? Color.red : Color.blue)
+                            }
                         }
-                        Spacer()
                     }
                 }
             }
@@ -38,8 +50,11 @@ struct AirportSelectionView: View {
     }
 }
 
+
 struct AirportSelectionView_Previews: PreviewProvider {
     static var previews: some View {
-        AirportSelectionView()
+        let airport = Airport(name: "Test", iataCode: "Test", cityName: "Test", countryName: "Test")
+        
+        AirportSelectionView(selectedAirport: .constant(airport))
     }
 }
--- Ceyehat/Views/FlightSearchView.swift
+++ Ceyehat/Views/FlightSearchView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct FlightSearchView: View {
     @State private var selectedFlightType = 0
+    @State private var selectedDepartureAirport: Airport? = nil
+    @State private var selectedArrivalAirport: Airport? = nil
+    @State private var isSelectingDepartureAirport: Bool = false
+    @State private var isSelectingArrivalAirport: Bool = false
     
     let flightTypes = ["One way", "Round trip", "Multi-city"]
     
@@ -23,13 +27,25 @@ struct FlightSearchView: View {
                 .pickerStyle(SegmentedPickerStyle())
             }
             Section(header: Text("Where would you like to go?")) {
-                NavigationLink(destination: AirportSelectionView()) {
-                    Label("From", systemImage: "airplane.departure")
+                Button(action: {
+                    isSelectingDepartureAirport = true
+                }) {
+                    Label(selectedDepartureAirport != nil ? selectedDepartureAirport!.iataCode : "From", systemImage: "airplane.departure")
+                        .foregroundColor(selectedDepartureAirport != nil ? .blue : .black)
                 }
-                NavigationLink(destination: AirportSelectionView()) {
-                    Label("To", systemImage: "airplane.arrival")
+                Button(action: {
+                    isSelectingArrivalAirport = true
+                }) {
+                    Label(selectedArrivalAirport != nil ? selectedArrivalAirport!.iataCode : "To", systemImage: "airplane.arrival")
+                        .foregroundColor(selectedArrivalAirport != nil ? .blue : .black)
                 }
             }
+        }
+        .sheet(isPresented: $isSelectingDepartureAirport) {
+            AirportSelectionView(selectedAirport: $selectedDepartureAirport)
+        }
+        .sheet(isPresented: $isSelectingArrivalAirport) {
+            AirportSelectionView(selectedAirport: $selectedArrivalAirport)
         }
     }
 }
