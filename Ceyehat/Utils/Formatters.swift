7e94f461d2e0a896ebb89c4b66f89083fc87e342
ademclk <ademonurcelik@icloud.com>
2023-05-07 15:16:35 +0300

    Add necessary formatters

--- Ceyehat/Utils/Formatters.swift
+++ Ceyehat/Utils/Formatters.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+var dateFormatter: DateFormatter {
+    let formatter = DateFormatter()
+    formatter.locale = Locale.current
+    formatter.dateFormat = "HH:mm"
+    return formatter
+}
+
+var birthDateFormater: DateFormatter {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "tr_TR")
+    formatter.dateFormat = "dd MMMM yyyy"
+    return formatter
+}
+
+var dateSelectionFormatter: DateFormatter {
+    let formatter = DateFormatter()
+    var calendar = Calendar(identifier: .gregorian)
+    calendar.locale = Locale(identifier: "tr_TR")
+    formatter.calendar = calendar
+    formatter.dateFormat = "MMM d"
+    return formatter
+}
+
+var durationFormatter: DateComponentsFormatter {
+    let formatter = DateComponentsFormatter()
+    var calendar = Calendar(identifier: .gregorian)
+    calendar.locale = Locale(identifier: "tr_TR")
+    formatter.calendar = calendar
+    formatter.unitsStyle = .short
+    formatter.allowedUnits = [.hour, .minute]
+    return formatter
+}
+
+var durationFormatterFull: DateComponentsFormatter {
+    let formatter = DateComponentsFormatter()
+    var calendar = Calendar(identifier: .gregorian)
+    calendar.locale = Locale(identifier: "tr_TR")
+    formatter.calendar = calendar
+    formatter.unitsStyle = .full
+    formatter.allowedUnits = [.hour, .minute]
+    return formatter
+}
