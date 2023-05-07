7c3409578d2c28290c0bf6330cc3f24d304c815f
ademclk <ademonurcelik@icloud.com>
2023-05-07 15:04:22 +0300

    Change entrance view

--- Ceyehat/CeyehatApp.swift
+++ Ceyehat/CeyehatApp.swift
@@ -13,7 +13,7 @@ struct CeyehatApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
