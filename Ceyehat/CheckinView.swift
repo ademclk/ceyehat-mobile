c61662f0c09306b546f7f36843c6d5d17d744c07
ademclk <ademonurcelik@icloud.com>
2023-05-07 14:34:05 +0300

    Add CheckinView

---
+++ Ceyehat/CheckinView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckinView.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 7.05.2023.
+//
+
+import SwiftUI
+
+struct CheckinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckinView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckinView()
+    }
+}
