c8b3dab781ffda39ff4e95f47787d82f5bb68dfe
ademclk <ademonurcelik@icloud.com>
2023-05-07 14:41:59 +0300

    # Changed WelcomeView to display a greeting and navigation buttons
    Changed the greeting message added buttons
    
    * Added a waving hand image to the greeting message.
    * Updated the navigation links to use Turkish text.
    * Added navigation buttons

--- Ceyehat/Views/WelcomeView.swift
+++ Ceyehat/Views/WelcomeView.swift
@@ -7,9 +7,65 @@
 
 import SwiftUI
 
+/// WelcomeView is the first view presented to the user when they launch the app.
+///
+/// The view contains a greeting message and two buttons that allow the user to
+/// navigate to flight booking or check-in sections.
+///
+/// - Note: The view uses a NavigationView to provide navigation functionality.
+/// - SeeAlso: `FlightSearchView`, `CheckInView`
 struct WelcomeView: View {
+    @State private var isShowingFlightSearch = false
+    
+    /// The body property defines the visual appearance of the view.
+    ///
+    /// It consists of the following subviews:
+    /// - A greeting text ("Merhaba") and a waving hand image.
+    /// - Two buttons for navigation to flight booking and check-in sections.
+    ///
+    /// - Returns: A ScrollView containing the VStack with the main content.
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVStack(spacing: 20) {
+                    HStack(spacing: 15) {
+                        Text("Merhaba")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Image(systemName: "figure.wave")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20)
+                    }
+                    .padding()
+                    VStack(spacing: 30) {
+                        NavigationLink(destination: FlightSearchView()) {
+                            Text("Bilet al")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        .padding()
+                        NavigationLink(destination: CheckinView()) {
+                            Text("Check-in")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        .padding()
+                    }
+                    .padding()
+                }
+                .navigationTitle("Ceyehat")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+        }
     }
 }
 
