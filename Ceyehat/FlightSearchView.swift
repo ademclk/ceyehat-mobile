e5830deba6a7a34a23058f2f54e37759b7052aed
ademclk <ademonurcelik@icloud.com>
2023-05-07 14:33:32 +0300

    Add FlightSearchView

---
+++ Ceyehat/FlightSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  FlightSearchView.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 7.05.2023.
+//
+
+import SwiftUI
+
+struct FlightSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlightSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightSearchView()
+    }
+}
