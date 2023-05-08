e6e3b41cbb2351aa6cadaf29789dc27bc5f6820e
ademclk <ademonurcelik@icloud.com>
2023-05-09 00:12:36 +0300

    Add BookingView

---
+++ Ceyehat/Views/Customer/Booking/BookingView.swift
@@ -0,0 +1,20 @@
+//
+//  BookingView.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 5.04.2023.
+//
+
+import SwiftUI
+
+struct BookingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookingView()
+    }
+}
