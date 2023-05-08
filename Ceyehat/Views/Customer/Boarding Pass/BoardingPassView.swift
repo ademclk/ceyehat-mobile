648f9bf966a6fc6da6e53d5108e8f2495d3eb27e
ademclk <ademonurcelik@icloud.com>
2023-05-09 00:12:50 +0300

    Add BoardingPassView

---
+++ Ceyehat/Views/Customer/Boarding Pass/BoardingPassView.swift
@@ -0,0 +1,20 @@
+//
+//  BoardingPassView.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 5.04.2023.
+//
+
+import SwiftUI
+
+struct BoardingPassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BoardingPassView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardingPassView()
+    }
+}
