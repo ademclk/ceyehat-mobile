a1696be8e4f113babd57c9e52f250218386279c2
ademclk <ademonurcelik@icloud.com>
2023-05-10 20:57:13 +0300

    Add Category model

---
+++ Ceyehat/Models/Category.swift
@@ -0,0 +1,21 @@
+//
+//  Category.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 10.05.2023.
+//
+
+import Foundation
+
+struct Category: Identifiable {
+    let id = UUID()
+    let name: String
+    let iconName: String
+}
+
+let sampleCategories = [
+    Category(name: "Tarihi Yerler", iconName: "clock.arrow.circlepath"),
+    Category(name: "Plajlar", iconName: "umbrella"),
+    Category(name: "Doğa Aktiviteleri", iconName: "leaf.arrow.circlepath"),
+    Category(name: "Gastronomi", iconName: "fork.knife.circle")
+]
