8c2c37709fd4272f1c2b3a4bd96521d225efe77e
ademclk <ademonurcelik@icloud.com>
2023-05-10 20:57:47 +0300

    Add CityMapView, disabled interactions

---
+++ Ceyehat/Utils/CityMapView.swift
@@ -0,0 +1,75 @@
+//
+//  CityMapView.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 10.05.2023.
+//
+
+import Foundation
+import SwiftUI
+import MapKit
+
+struct CityMapView: UIViewRepresentable {
+    let coordinate: CLLocationCoordinate2D
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        mapView.isZoomEnabled = false
+        mapView.isScrollEnabled = false
+        mapView.isUserInteractionEnabled = false
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: CityMapView
+        
+        init(_ parent: CityMapView) {
+            self.parent = parent
+        }
+    }
+}
+
+
+struct CityMapModalView: View {
+    let city: FeaturedCity?
+    @Binding var showingCityMap: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    showingCityMap = false
+                }
+            
+            VStack {
+                Text(city?.name ?? "")
+                    .font(.headline)
+                    .padding()
+                
+                if let coordinate = city?.coordinate {
+                    MapView(coordinate: coordinate)
+                        .frame(height: 300)
+                        .cornerRadius(10)
+                        .padding()
+                }
+            }
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(.horizontal)
+        }
+    }
+}
