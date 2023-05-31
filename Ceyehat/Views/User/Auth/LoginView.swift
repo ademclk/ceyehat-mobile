59cd32bc56f6092dcb3bf99a5960daedd301dd01
ademclk <ademonurcelik@icloud.com>
2023-05-31 13:38:05 +0300

    Modify auth related views

--- Ceyehat/Views/User/Auth/LoginView.swift
+++ Ceyehat/Views/User/Auth/LoginView.swift
@@ -8,41 +8,72 @@
 import SwiftUI
 
 struct LoginView: View {
-    @StateObject private var viewModel = LoginViewModel()
+    @State private var showingAlert: Bool = false
+    @EnvironmentObject var userAuth: UserAuth
+    @StateObject var loginViewModel: LoginViewModel
+    
+    init(userAuth: UserAuth) {
+        _loginViewModel = StateObject(wrappedValue: LoginViewModel(userAuth: userAuth))
+    }
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Giriş Yap")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-            
+        NavigationView {
             VStack {
-                TextField("Email", text: $viewModel.email)
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                    .padding()
-                    .background(.thickMaterial)
-                    .cornerRadius(10)
-                    .disableAutocorrection(true)
+                Text("Giriş Yap")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 20)
                 
-                SecureField("Password", text: $viewModel.password)
-                    .padding()
-                    .background(.thickMaterial)
-                    .cornerRadius(10)
+                VStack(alignment: .leading) {
+                    Text("E-posta")
+                        .font(.headline)
+                    TextField("E-posta adresiniz", text: $loginViewModel.email)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.emailAddress)
+                    
+                    Text("Şifre")
+                        .font(.headline)
+                    SecureField("Şifreniz", text: $loginViewModel.password)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    
+                    Button(action: {
+                        loginViewModel.login()
+                    }) {
+                        Text("Giriş Yap")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Giriş Hatası"), message: Text("E-posta veya şifre yanlış."), dismissButton: .default(Text("Tamam")))
+                    }
+                }
+                .padding()
+                NavigationLink(destination: RegisterView()) {
+                    Text("Hesabınız yok mu? Kayıt Ol")
+                        .foregroundColor(.blue)
+                }
+                .padding(.top, 15)
             }
-            
-        
-            Spacer()
         }
-        .padding()
-        .background(.red.opacity(0.8))
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        let userAuth = UserAuth()
+        LoginView(userAuth: userAuth)
+            .environmentObject(userAuth)
     }
 }
+
--- Ceyehat/Views/User/Auth/RegisterView.swift
+++ Ceyehat/Views/User/Auth/RegisterView.swift
@@ -8,10 +8,76 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @StateObject var registerViewModel = RegisterViewModel()
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Hesap Oluştur")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                VStack(alignment: .leading) {
+                    Text("Ad")
+                        .font(.headline)
+                    TextField("Adınız", text: $registerViewModel.firstName)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    
+                    Text("Soyad")
+                        .font(.headline)
+                    TextField("Soyadınız", text: $registerViewModel.lastName)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    
+                    Text("E-posta")
+                        .font(.headline)
+                    TextField("E-posta adresiniz", text: $registerViewModel.email)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.emailAddress)
+                    
+                    Text("Şifre")
+                        .font(.headline)
+                    SecureField("Şifreniz", text: $registerViewModel.password)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    
+                    Button(action: {
+                        registerViewModel.register()
+                    }) {
+                        Text("Kayıt Ol")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .alert(isPresented: $registerViewModel.isRegistered) {
+                        Alert(title: Text("Kayıt Başarılı"), message: Text("Kayıt işlemi başarıyla tamamlandı."), dismissButton: .default(Text("Tamam")))
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Hata"), message: Text(registerViewModel.errorMessage), dismissButton: .default(Text("Tamam")))
+                    }
+                }
+                .padding()
+            }
+        }
     }
 }
+
+
 
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
