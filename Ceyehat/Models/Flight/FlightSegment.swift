8015e7bbc6a00efa93c69ff9bd0c1d0197279799
ademclk <ademonurcelik@icloud.com>
2023-06-06 15:08:53 +0300

    Add FlightSegment model

---
+++ Ceyehat/Models/Flight/FlightSegment.swift
@@ -0,0 +1,13 @@
+//
+//  FlightSegment.swift
+//  Ceyehat
+//
+//  Created by Adem Onur Çelik on 4.06.2023.
+//
+
+import Foundation
+
+struct FlightSegment: Identifiable {
+    let id = UUID()
+    let ticket: FlightTicket
+}
